e0bee6e2d3973cabeb6aeeac2f8821b078cef314
Vineet032 <vk73511390@gmail.com>
2023-08-02 06:04:45 +0530

    hello world message

--- testcode.js
+++ testcode.js
@@ -1,14 +1,1 @@
-let mouseY = 0;
-   function handleMouseMove(event) {
-        const { clientY } = event;
-        const { innerHeight } = window;
-        mouseY = (clientY / innerHeight) * 2 - 1;
-
-        const moveSpeed = 0.5; 
-        const maxCameraPositionY = 50;
-
-        const cameraPositionY = moveSpeed * mouseY * maxCameraPositionY;
-        camera.position.y = Math.min(Math.max(cameraPositionY, -maxCameraPositionY), maxCameraPositionY);
-      }
-
-      document.addEventListener('mousemove', handleMouseMove);
+console.log("hello world!");
